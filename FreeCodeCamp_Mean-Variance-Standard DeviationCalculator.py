8847d673b17172a2b7e7baf25c81111804a706e2
Trần Tú Vân Hà <51289206+vanha1711@users.noreply.github.com>
2021-09-14 11:42:52 +0700

    Update FreeCodeCamp_Mean-Variance-Standard DeviationCalculator.py

--- FreeCodeCamp_Mean-Variance-Standard DeviationCalculator.py
+++ FreeCodeCamp_Mean-Variance-Standard DeviationCalculator.py
@@ -1,42 +1,19 @@
 import numpy as np
 
 def calculate(list):
-    if len(list) < 9:
-        print("List must contain nine numbers.")
+    if len(list) != 9:
+        raise ValueError("List must contain nine numbers.")
     else:
-        mask = np.array(list).reshape(3, 3)
+        list = np.array(list).reshape((3, 3))
 
-        # row
-        mean_row = mask.mean(axis=1).tolist()
-        var_row = mask.var(axis=1).tolist()
-        std_row = mask.std(axis=1).tolist()
-        max_row = mask.max(axis=1).tolist()
-        min_row = mask.min(axis=1).tolist()
-        sum_row = mask.sum(axis=1).tolist()
+        mean = [list.mean(axis=0).tolist(), list.mean(axis=1).tolist(), list.mean().tolist()]
+        var = [list.var(axis=0).tolist(), list.var(axis=1).tolist(), list.var().tolist()]
+        std = [list.std(axis=0).tolist(), list.std(axis=1).tolist(), list.std().tolist()]
+        max = [list.max(axis=0).tolist(), list.max(axis=1).tolist(), list.max().tolist()]
+        min = [list.min(axis=0).tolist(), list.min(axis=1).tolist(), list.min().tolist()]
+        sum = [list.sum(axis=0).tolist(), list.sum(axis=1).tolist(), list.sum().tolist()]
 
-        # column
-        mean_col = mask.mean(axis=0).tolist()
-        var_col = mask.var(axis=0).tolist()
-        std_col = mask.std(axis=0).tolist()
-        max_col = mask.max(axis=0).tolist()
-        min_col = mask.min(axis=0).tolist()
-        sum_col = mask.sum(axis=0).tolist()
-
-        # element
-        mean_ele = mask.mean().tolist()
-        var_ele = mask.var().tolist()
-        std_ele = mask.std().tolist()
-        max_ele = mask.max().tolist()
-        min_ele = mask.min().tolist()
-        sum_ele = mask.sum().tolist()
-
-        keys = ['mean', 'variance', 'standard deviation', 'max', 'min', 'sum']
-
-        values = [[mean_row, mean_col, mean_ele], [var_row, var_col, var_ele], [std_row, std_col, std_ele],
-                  [max_row, max_col, max_ele], [min_row, min_col, min_ele], [sum_row, sum_col, sum_ele]]
-
-        calculations = dict(zip(keys, values))
+        calculations = {"mean": mean, "variance": var, "standard deviation": std, "max": max, "min":min, "sum":sum}
 
     return calculations
 
-calculate([0,1,2,3,4,5,6,7,8])
